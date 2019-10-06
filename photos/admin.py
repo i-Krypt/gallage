fd7770a544febac92408353a43e7ea8a59a6df84
i-Krypt <vamay505@gmail.com>
2019-10-06 21:42:59 +0300

    registerd models to the admin file

--- photos/admin.py
+++ photos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Uploader,Uploads,tags
 
 # Register your models here.
+admin.site.register(Uploader)
+admin.site.register(Uploads)
+admin.site.register(tags)
