891bb2dee46489f5d0287cf54a5ec3c444dfba84
i-Krypt <vamay505@gmail.com>
2019-10-06 17:06:01 +0300

    created an uploader class

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Uploader(models.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    email = models.EmailField()
+
+
+    def __str__(self):
+        return self.first_name
+    class Meta:
+        ordering = ['first_name']
