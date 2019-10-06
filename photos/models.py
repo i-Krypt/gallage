2a85e886316ea19ddef39a1897466b64cf38da96
i-Krypt <vamay505@gmail.com>
2019-10-06 17:46:40 +0300

    added a tags model

--- photos/models.py
+++ photos/models.py
@@ -11,3 +11,10 @@ class Uploader(models.Model):
         return self.first_name
     class Meta:
         ordering = ['first_name']
+
+
+class tags(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
