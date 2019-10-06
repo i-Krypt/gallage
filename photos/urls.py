aa25b4d6e28d1b54bc15704405f9b56706a2c8b6
i-Krypt <vamay505@gmail.com>
2019-10-06 22:41:32 +0300

    updated the urls and sert todays_photos as root

--- photos/urls.py
+++ photos/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$',views.index,name = 'index'), 
-    url('^today/$',views.todays_photos,name='photosToday')
+    url('^today/$',views.todays_photos,name='photosToday'),
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_photos,name = 'pastPhotos')
+
 ]
