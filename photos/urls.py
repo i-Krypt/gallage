20161074015aeb5304973b23e4737dd2c8fb1cdb
i-Krypt <vamay505@gmail.com>
2019-10-07 00:33:40 +0300

    updated the todays_photos view to be the site root

--- photos/urls.py
+++ photos/urls.py
@@ -2,7 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^today/$',views.todays_photos,name='photosToday'),
-    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_photos,name = 'pastPhotos')
-
+      url(r'^$',views.todays_photos,name = 'photosToday'),
 ]
