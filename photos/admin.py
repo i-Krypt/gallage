962f420eb5cd5aba2358206753eb77721906103a
i-Krypt <vamay505@gmail.com>
2019-10-07 00:58:17 +0300

    added a Uploadsadmin class

--- photos/admin.py
+++ photos/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import Uploader,Uploads,tags
 
 # Register your models here.
+class UploadsAdmin(admin.ModelAdmin):
+    filter_horizontal =('tags',)
+
 admin.site.register(Uploader)
 admin.site.register(Uploads)
 admin.site.register(tags)
